776d9ccdb00be0ca9ce294ed16609cb764b03d78
prem <prembudda@gmail.com>
2021-11-18 22:55:28 -0800

    Update HelloWorldService.java

--- src/main/java/com/rst/helloworld/service/HelloWorldService.java
+++ src/main/java/com/rst/helloworld/service/HelloWorldService.java
@@ -23,7 +23,7 @@ public class HelloWorldService {
 		logger.debug("getTitle() is executed! $name : {}", name);
 
 		if(StringUtils.isEmpty(name)){
-			return "Hello Docker";
+			return "Hi Docker";
 		}else{
 			return "Hello " + name;
 		}
